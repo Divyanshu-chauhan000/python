822d2df4c9665a6469aa906e6c61f9ada86692f0
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2025-03-17 19:26:20 +0530

    Function and Recursion added

--- Functions/fn.py
+++ Functions/fn.py
@@ -1,9 +1,27 @@
-def avg():
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    c = int(input("Enter third number: "))
-    average = (a + b + c) / 3
-    print("The average of the three numbers is: ", average)
+# Function definition
+# def avg():
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     c = int(input("Enter third number: "))
+#     average = (a + b + c) / 3
+#     print("The average of the three numbers is: ", average)
+
+# #  Function call
+# avg()
 
 
-avg()
+# def greet(name):
+#     print("Hello, Good Morning"+ " " +name)
+#     return "ok"
+
+
+# a= greet("Dev")
+# print(a)
+
+
+def greet(name , msg = "Good morning"):
+    print(f"hello , {name}")
+    print(msg)
+
+greet("Divyanshu" , "Good Night") 
+greet("Dev") # Default argument   
